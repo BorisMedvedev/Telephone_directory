21448f8a8a1907e5f2715807de39c44fae3c13b8
John Doe <johndoe@example.com>
2023-03-15 08:26:22 +0300

    fix

--- phonebook/js/main.js
+++ phonebook/js/main.js
@@ -5,14 +5,16 @@ import { data } from '../../data.js';
 {
   const createContainer = () => {
     const container = document.createElement('div');
+
     container.classList.add('container');
     return container;
   };
 
   const createHeader = () => {
     const header = document.createElement('header');
-    header.classList.add('header');
     const headerContainer = createContainer();
+
+    header.classList.add('header');
     header.append(headerContainer);
     header.headerContainer = headerContainer;
     return header;
@@ -20,6 +22,7 @@ import { data } from '../../data.js';
 
   const createLogo = (title) => {
     const h1 = document.createElement('h1');
+
     h1.classList.add('logo');
     h1.textContent = `Телефонный справочник. Пользователь - ${title}`;
     return h1;
@@ -27,8 +30,9 @@ import { data } from '../../data.js';
 
   const createMain = () => {
     const main = document.createElement('main');
-    main.classList.add('main');
     const mainContainer = createContainer();
+
+    main.classList.add('main');
     main.mainContainer = mainContainer;
     main.append(mainContainer);
     return main;
@@ -40,6 +44,7 @@ import { data } from '../../data.js';
 
     const btns = params.map(({ className, type, text }) => {
       const button = document.createElement('button');
+
       button.type = type;
       button.className = className;
       button.textContent = text;
@@ -54,15 +59,16 @@ import { data } from '../../data.js';
 
   const createTable = () => {
     const table = document.createElement('table');
-    table.classList.add('table', 'table-striped');
     const thead = document.createElement('thead');
+
+    table.classList.add('table', 'table-striped');
     thead.insertAdjacentHTML(
       'beforeend',
       `
     <tr>
        <th class='delete'>Удалить</th>
-       <th>Имя</th>
-       <th>Фамилия</th>
+       <th class='th-name'>Имя</th>
+       <th class='th-surname'>Фамилия</th>
        <th>Телефон</th>
        <th></th>
     </tr>
@@ -70,6 +76,7 @@ import { data } from '../../data.js';
     );
 
     const tbody = document.createElement('tbody');
+
     table.append(thead, tbody);
     table.tbody = tbody;
     return table;
@@ -77,8 +84,9 @@ import { data } from '../../data.js';
 
   const createForm = () => {
     const overlay = document.createElement('div');
-    overlay.classList.add('form-overlay');
     const form = document.createElement('form');
+
+    overlay.classList.add('form-overlay');
     form.classList.add('form');
     form.insertAdjacentHTML(
       'beforeend',
@@ -123,6 +131,7 @@ import { data } from '../../data.js';
   const createFooter = () => {
     const footer = document.createElement('footer');
     const copy = document.createElement('span');
+
     copy.textContent = `Все права защищены ©Boris`;
     footer.append(copy);
     footer.classList.add('footer');
@@ -146,8 +155,10 @@ import { data } from '../../data.js';
       },
     ]);
     const table = createTable();
+
     const form = createForm();
     const footer = createFooter();
+
     header.headerContainer.append(logo);
     main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
     app.append(header, main, footer);
@@ -164,19 +175,20 @@ import { data } from '../../data.js';
 
   const createRow = ({ name: firstName, surname, phone }) => {
     const tr = document.createElement('tr');
-    tr.classList.add('contact');
     const tdDel = document.createElement('td');
     const tdName = document.createElement('td');
-    tdName.textContent = firstName;
     const tdSurename = document.createElement('td');
-    tdSurename.textContent = surname;
     const tdPhone = document.createElement('td');
     const tdEdit = document.createElement('td');
     const linkPhone = document.createElement('a');
-    linkPhone.href = `tel:${phone}`;
-    linkPhone.textContent = phone;
     const buttonDel = document.createElement('button');
     const buttonEdit = document.createElement('button');
+
+    tr.classList.add('contact');
+    tdName.textContent = firstName;
+    tdSurename.textContent = surname;
+    linkPhone.href = `tel:${phone}`;
+    linkPhone.textContent = phone;
     buttonEdit.title = 'Редактировать';
     buttonEdit.classList.add('btn-edit');
     tdPhone.append(linkPhone);
@@ -206,6 +218,14 @@ import { data } from '../../data.js';
       });
     });
   };
+
+  function sortTable(arr, prop, dir) {
+    const arrCopy = [...arr];
+    return arrCopy.sort(function (stA, stB) {
+      if (!dir == false ? stA[prop] < stB[prop] : stA[prop] > stB[prop])
+        return -1;
+    });
+  }
 
   const init = (selector, title) => {
     const app = document.querySelector(selector);
@@ -240,14 +260,29 @@ import { data } from '../../data.js';
       }
     });
 
-    setTimeout(() => {
-      const contact = createRow({
-        name: 'Борис',
-        surname: 'Медведев',
-        phone: '+79876543210',
-      });
-      list.append(contact);
-    }, 3000);
+    // setTimeout(() => {
+    //   const contact = createRow({
+    //     name: 'Борис',
+    //     surname: 'Медведев',
+    //     phone: '+79876543210',
+    //   });
+    //   list.append(contact);
+    // }, 1000);
+
+    let dir = true;
+    const thName = document.querySelector('.th-name');
+    thName.addEventListener('click', () => {
+      list.innerHTML = '';
+      renderContacts(list, sortTable(data, 'name', dir));
+      dir = !dir;
+    });
+
+    const thSurName = document.querySelector('.th-surname');
+    thSurName.addEventListener('click', () => {
+      list.innerHTML = '';
+      renderContacts(list, sortTable(data, 'name', dir));
+      dir = !dir;
+    });
   };
 
   window.phoneBookInit = init;
